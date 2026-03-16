33582dccb275d7915d3f79eaa1ac9eba8633379e
Jaime Garcia Marin <jaime.garcia1@alu.uhu.es>
2026-03-16 09:01:11 +0100

    Commit Inicial

---
+++ mgcss-track-L2-Grupo4/src/test/java/com/mgcss/domain/MgcssTrackL2Grupo4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mgcss.domain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MgcssTrackL2Grupo4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
